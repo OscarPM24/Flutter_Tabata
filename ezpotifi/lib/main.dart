0ff949b70ff769268ac048000b1f60e3399978c6
OscarPM24 <2023_oscar.perez@iticbcn.cat>
2024-01-24 20:49:29 +0100

    timer choto

--- ezpotifi/lib/main.dart
+++ ezpotifi/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
+import 'dart:async';
 
 void main() => runApp(MyApp());
 
@@ -9,6 +10,47 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  Timer? _timer;
+  int _remainingSeconds;
+
+  _MyAppState({int startSeconds = 4}) : _remainingSeconds = startSeconds;
+
+  void _startCountdown() {
+    const time = Duration(seconds: 4);
+
+    _timer = Timer.periodic(time, (Timer timer) {
+      if (_remainingSeconds <= 0) {
+        setState(() {
+          resetTimer();
+        });
+      } else {
+        setState(() {
+          _remainingSeconds--;
+        });
+      }
+    });
+  }
+
+  void resetTimer() {
+    if (_timer!.isActive) {
+      _timer?.cancel();
+    }
+
+    _timer = Timer.periodic(Duration(seconds: 4), (Timer timer) {
+      _remainingSeconds = 4;
+      // _startCountdown();
+
+      print('Timer has finished counting down after reset.');
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,6 +59,15 @@ class _MyAppState extends State<MyApp> {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              Text(
+                '$_remainingSeconds',
+                style: TextStyle(fontSize: 48),
+              ),
+              FloatingActionButton(
+                onPressed: _startCountdown,
+                tooltip: 'Start Countdown',
+                child: Icon(Icons.timer),
+              ),
               IconButton.filled(
                   icon: const Icon(Icons.arrow_right),
                   onPressed: () {
