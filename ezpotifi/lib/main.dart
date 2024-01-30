b710a698080363d3cb2a2a224f419271f61286e7
Adgar90 <adriagp90@gmail.com>
2024-01-30 20:53:32 +0100

    afegit circular timer

--- ezpotifi/lib/main.dart
+++ ezpotifi/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'dart:async';
+import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,46 +11,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  Timer? _timer;
-  int _remainingSeconds;
-
-  _MyAppState({int startSeconds = 4}) : _remainingSeconds = startSeconds;
-
-  void _startCountdown() {
-    const time = Duration(seconds: 4);
-
-    _timer = Timer.periodic(time, (Timer timer) {
-      if (_remainingSeconds <= 0) {
-        setState(() {
-          resetTimer();
-        });
-      } else {
-        setState(() {
-          _remainingSeconds--;
-        });
-      }
-    });
-  }
-
-  void resetTimer() {
-    if (_timer!.isActive) {
-      _timer?.cancel();
-    }
-
-    _timer = Timer.periodic(Duration(seconds: 4), (Timer timer) {
-      _remainingSeconds = 4;
-      // _startCountdown();
-
-      print('Timer has finished counting down after reset.');
-    });
-  }
-
-  @override
-  void dispose() {
-    _timer?.cancel();
-
-    super.dispose();
-  }
+  final int _duration = 10;
+  final CountDownController _controller = CountDownController();
 
   @override
   Widget build(BuildContext context) {
@@ -61,12 +24,108 @@ class _MyAppState extends State<MyApp> {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(
-                '$_remainingSeconds',
-                style: TextStyle(fontSize: 48),
+              CircularCountDownTimer(
+                // Countdown duration in Seconds.
+                duration: _duration,
+
+                // Countdown initial elapsed Duration in Seconds.
+                initialDuration: 0,
+
+                // Controls (i.e Start, Pause, Resume, Restart) the Countdown Timer.
+                controller: _controller,
+
+                // Width of the Countdown Widget.
+                width: MediaQuery.of(context).size.width / 2,
+
+                // Height of the Countdown Widget.
+                height: MediaQuery.of(context).size.height / 2,
+
+                // Ring Color for Countdown Widget.
+                ringColor: Colors.grey[300]!,
+
+                // Ring Gradient for Countdown Widget.
+                ringGradient: null,
+
+                // Filling Color for Countdown Widget.
+                fillColor: Colors.purpleAccent[100]!,
+
+                // Filling Gradient for Countdown Widget.
+                fillGradient: null,
+
+                // Background Color for Countdown Widget.
+                backgroundColor: Colors.purple[500],
+
+                // Background Gradient for Countdown Widget.
+                backgroundGradient: null,
+
+                // Border Thickness of the Countdown Ring.
+                strokeWidth: 20.0,
+
+                // Begin and end contours with a flat edge and no extension.
+                strokeCap: StrokeCap.round,
+
+                // Text Style for Countdown Text.
+                textStyle: const TextStyle(
+                  fontSize: 33.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+
+                // Format for the Countdown Text.
+                textFormat: CountdownTextFormat.S,
+
+                // Handles Countdown Timer (true for Reverse Countdown (max to 0), false for Forward Countdown (0 to max)).
+                isReverse: false,
+
+                // Handles Animation Direction (true for Reverse Animation, false for Forward Animation).
+                isReverseAnimation: false,
+
+                // Handles visibility of the Countdown Text.
+                isTimerTextShown: true,
+
+                // Handles the timer start.
+                autoStart: false,
+
+                // This Callback will execute when the Countdown Starts.
+                onStart: () {
+                  // Here, do whatever you want
+                  debugPrint('Countdown Started');
+                },
+
+                // This Callback will execute when the Countdown Ends.
+                onComplete: () {
+                  // Here, do whatever you want
+                  debugPrint('Countdown Ended');
+                },
+
+                // This Callback will execute when the Countdown Changes.
+                onChange: (String timeStamp) {
+                  // Here, do whatever you want
+                  debugPrint('Countdown Changed $timeStamp');
+                },
+
+                /* 
+            * Function to format the text.
+            * Allows you to format the current duration to any String.
+            * It also provides the default function in case you want to format specific moments
+              as in reverse when reaching '0' show 'GO', and for the rest of the instances follow 
+              the default behavior.
+          */
+                timeFormatterFunction: (defaultFormatterFunction, duration) {
+                  if (duration.inSeconds == 0) {
+                    // only format for '0'
+                    return "Start";
+                  } else {
+                    // other durations by it's default format
+                    return Function.apply(defaultFormatterFunction, [duration]);
+                  }
+                },
               ),
               FloatingActionButton(
-                onPressed: _startCountdown,
+                onPressed: () {
+                  _controller.start();
+                  player.play(AssetSource('sound.mp3'));
+                },
                 tooltip: 'Start Countdown',
                 child: Icon(Icons.timer),
               ),
